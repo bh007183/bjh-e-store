ef0feb78bc1cb98f458768395a7b9b7229ea5dc8
Ben Hopkins <bjhops17@gmail.com>
2021-03-20 08:27:46 -0700

    added missing npms

--- src/components/ShoppingNav/index.js
+++ src/components/ShoppingNav/index.js
@@ -3,6 +3,7 @@ import Button from "@material-ui/core/Button";
 import AppBar from "@material-ui/core/AppBar";
 import Hidden from "@material-ui/core/Hidden";
 import Toolbar from "@material-ui/core/Toolbar";
+import { makeStyles } from '@material-ui/core/styles';
 
 import Typography from "@material-ui/core/Typography";
 
